da8ef1fc4f01c9287dbbe31684febcbbbe3962ef
yudaginting766 <129074159+yudaginting766@users.noreply.github.com>
2023-03-28 15:19:21 +0700

    Add files via upload

---
+++ aplikasi.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>PEMJUMLAHAN</title>
+</head>
+
+<body>
+    <h5> APLIKASI PERHITUNGAN MATEMATIKA </h5>
+    <form action="proses.php" method="POST">
+        Bilangan 1 : <br>
+        <input type="text" name="txtbil1"><br>
+        Bilangan 2 : <br>
+        <input type="text" name="txtbil2"><br>
+        <button type="submit">HASIL</button>
+    </form>
+</body>
+</html>
---
+++ proses.php
@@ -0,0 +1,19 @@
+<?php 
+$no1 = $_POST['txtbil1'];
+$no2 = $_POST['txtbil2'];
+
+echo" Bilangan 1 :" .$no1. "<br>";
+echo" Bilangan 2 :" .$no2. "<br><br>";
+
+$tambah = $no1 + $no2 ;
+echo " Hasil Tambah = $tambah <br><br>";
+$kurang = $no1 - $no2 ; 
+echo " Hasil Kurang = $kurang <br><br>";
+$kali = $no1 * $no2 ; 
+echo " Hasil Perkalian = $kali<br><br>";
+$bagi = $no1 / $no2 ; 
+echo " Hasil Kurang = $bagi<br><br>";
+//()
+//{}
+//[]
+?>
